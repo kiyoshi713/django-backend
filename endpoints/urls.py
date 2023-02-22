7d961655c2739b10298eaec92e1bd29d7b51ae4e
kiyoshi713 <112112870+kiyoshi713@users.noreply.github.com>
2023-02-22 16:46:09 -0500

    c

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -4,10 +4,9 @@ from . import views
 urlpatterns = [
     path("loginCliente", views.loginCliente),
     path("cuestionario", views.Formulario),
-<<<<<<< HEAD
+
     path("loginRest", views.loginRestaurante),
-=======
+
     path("restaurant/listar", views.obtenerListarestaurant), 
-    path("restaurant/categorias", views.Mostrar_Catego)
->>>>>>> 38110f7bba922a6a3f45873ba74da7a9d77646c3
+    path("restaurant/categorias", views.Mostrar_Catego),
 ]
