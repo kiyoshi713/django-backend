5d51e0e18cc061daaa1118ef153c697677fdafb5
S0brina <solanosabrina78@gmail.com>
2023-02-24 23:09:05 -0500

    falta css

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("loginCliente", views.loginCliente),
     path("Carta", views.Carta),
     path("Categorias",views.CategoriasxRestaurante),
-
+    # path("Restaurantes", views.Restaurante),
 
     path("cuestionario", views.Formulario),
 
