8ac6d17699fa1127b6d8ba092fcfd4141d687a73
httpvalv <20192371@aloe.ulima.edu.pe>
2023-02-24 19:27:00 -0500

    .

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -4,10 +4,5 @@ from . import views
 urlpatterns = [
     path("loginCliente", views.loginCliente),
     path("cuestionario", views.Formulario),
-<<<<<<< HEAD
     path("loginRest", views.loginRestaurante),
-=======
-    path("restaurant/listar", views.obtenerListarestaurant), 
-    path("restaurant/categorias", views.Mostrar_Catego)
->>>>>>> 38110f7bba922a6a3f45873ba74da7a9d77646c3
 ]
