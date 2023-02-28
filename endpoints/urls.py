21c1a79fb5eca14cee25d3a76e8eb670b41b3f31
httpvalv <20192371@aloe.ulima.edu.pe>
2023-02-28 15:43:04 -0500

    123edd
    git commit -m 123edd

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path("Restaurantes", views.Restaurante),
     path("cuestionario", views.Formulario),
     path("loginRest", views.loginRestaurante),
-    path("Mostrar_ListaPedido", views.Mostrar_ListaPedido)
+    path("Mostrar_ListaPedido", views.Mostrar_ListaPedido),
+    path("registrarPedido",views.Registrar_EntregaPedido),
+    path("actualizarPedido",views.Actualizar_Pedido),
+    path("verificarPedido",views.Verificar_EstadoPedido)
 ]
