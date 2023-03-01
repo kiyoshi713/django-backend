a88814071ddc2eb071e6fd9300ef5ce44e45ed47
kiyoshi713 <112112870+kiyoshi713@users.noreply.github.com>
2023-03-01 13:21:54 -0500

    c

--- endpoints/models.py
+++ endpoints/models.py
@@ -60,5 +60,6 @@ class orden(models.Model):
     monto=models.DecimalField(decimal_places=2,max_digits=5)
     direccion=models.CharField(max_length=40)
     fecha=models.CharField(max_length=10)
+    estado=models.CharField(max_length=10)
     def __str__(self):
         return self.usuario
