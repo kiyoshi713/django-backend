d8f54ac2347d9043805d3dd849983bce4c35e153
kiyoshi713 <112112870+kiyoshi713@users.noreply.github.com>
2023-04-24 01:50:21 -0500

    parte

--- endpoints/models.py
+++ endpoints/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Cliente(models.Model):
+class Usuario(models.Model):
     username = models.CharField(max_length=25,primary_key=True)
     password = models.CharField(max_length=25)
 
